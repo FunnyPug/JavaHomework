cdd41ca5ad1063bcce647a03e2ff951f9dc4ead3
Ilya Moiseyev <jondorianpride@gmail.com>
2024-05-13 16:57:48 +0300

    fixed a problem with counting hits

--- chapter5/SimpleDotCom.java
+++ chapter5/SimpleDotCom.java
@@ -2,20 +2,33 @@ package com.javaHomework.chapter5;
 
 public class SimpleDotCom {
   int[] locationsCells;
-  int numOFHits = 0;
+  int[] strikeCells;
+  int numOFHits;
+  String result;
 
   public void setLocationsCells(int[] loc) {
     locationsCells = loc;
+    prepareStrikeCells(loc);
+  }
+
+  public void prepareStrikeCells(int[] validateCells) {
+    strikeCells = new int[validateCells.length];
+    for (int x = 0; x < strikeCells.length; x++) {
+      strikeCells[x] = -1;
+    }
   }
 
   public String checkYourself(String str) {
     int guess = Integer.parseInt(str);
-    String result = "Мимо";
-    for (int cell : locationsCells) {
-      if (guess == cell) {
-        result = "Попал";
-        numOFHits++;
-        break;
+    result = "Мимо";
+    if (!checkStrikeCells(guess)) {
+      for (int x = 0; x < locationsCells.length; x++) {
+        if (guess == locationsCells[x]) {
+          result = "Попал";
+          numOFHits++;
+          strikeCells[x] = guess;
+          break;
+        }
       }
     }
     if (numOFHits == locationsCells.length) {
@@ -23,5 +36,17 @@ public class SimpleDotCom {
     }
     System.out.println(result);
     return result;
+  }
+
+  public boolean checkStrikeCells(int shot) {
+    boolean isStrike = false;
+    for (int strikeCell : strikeCells) {
+      if (shot == strikeCell) {
+        result = "Ячейка с номером " + shot + " поражена, попробуйте другую";
+        isStrike = true;
+        break;
+      }
+    }
+    return isStrike;
   }
 }
